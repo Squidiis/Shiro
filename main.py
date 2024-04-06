fb831bbcaa2e9e190e0ba62d3f8e74f18553c9ca
Squidi <97200360+Squidiis@users.noreply.github.com>
2024-04-06 21:59:59 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -204,23 +204,6 @@ class AntiSpam(commands.Cog):
 bot.add_cog(AntiSpam(bot))
 
 
-class AutoReaction(commands.Cog):
-    def __init__(self, bot):
-        self.bot = bot
-
-    @commands.Cog.listener()
-    async def on_message(self, message:discord.Message):
-
-        if message.guild:
-            if message.guild.id == 865899808183287848:
-                categorys = [996846958536835203, 927683692695027772, 998934946708205598, 930157270275350598, 897544467266011177, 873622071484252200, 927668688239353926, 1084219817269149747]
-                if message.channel.category_id in categorys:
-                    if len(message.attachments) > 0 or message.content.startswith("https://"):
-                        await message.add_reaction("❤")
-        else:
-            return
-        
-bot.add_cog(AutoReaction(bot))
 
 
 
